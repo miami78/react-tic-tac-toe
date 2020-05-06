c3073ca514c22c95eec09773473968d9042a1187
miami78 <miamilarry78@gmail.com>
2020-05-06 12:59:14 +0300

    Updates

---
+++ precache-manifest.241b950afe2155e68b3729a79ee3a93e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3695b54a3c5b37281c77d7f3da7e159a",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "1d7210f806ecc9406621",
+    "url": "/react-tic-tac-toe/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "a21d82df8e6e19a064d6",
+    "url": "/react-tic-tac-toe/static/js/2.6873c516.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic-tac-toe/static/js/2.6873c516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d7210f806ecc9406621",
+    "url": "/react-tic-tac-toe/static/js/main.aac25083.chunk.js"
+  },
+  {
+    "revision": "db72ebdded73b8952fa1",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.04e26adc.js"
+  }
+]);
